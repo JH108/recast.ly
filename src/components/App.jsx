53897c070471dd51ad50aaa97cd63c03897c7f7b
Jesse Hill <jsnahill@gmail.com>
2017-02-25 18:49:03 -0800

    Implement almost all functionality, connection to youtube still not working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,51 @@
-var App = () => (
-  <div>
-    <Nav />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      currVideo: null
+    };
+  }
+
+
+  componentDidMount() {
+    this.getYouTubeVideos('react tutorials');
+  }
+
+  getYouTubeVideos(query) {
+    var options = {
+      key: this.props.API_KEY,
+      query: query
+    };
+    this.props.searchYouTube(options, (videos) =>
+      this.setState({
+        videos: videos,
+        currVideo: videos[0]
+      })
+    );
+  }
+
+  onTitleClick(video) {
+
+    this.setState({
+      currVideo: video
+    });
+  }
+
+  render() {
+    return (
+    <div>
+    <Nav handleSearchInputChange={this.getYouTubeVideos.bind(this)}/>
     <div className="col-md-7">
-      <VideoPlayer/>
+      <VideoPlayer video={this.state.currVideo}/>
     </div>
     <div className="col-md-5">
-      <VideoList/>
+      <VideoList videos={this.state.videos} onClickFunction={this.onTitleClick.bind(this)}/>
     </div>
   </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
